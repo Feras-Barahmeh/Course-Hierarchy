449939c2b95963a5cbd60a5bc78316b52900387e
Feras Barahmeh <ferasbarahmeh55@gmail.com>
2023-07-23 00:23:37 +0300

    update Autoload.php class

--- app/core/Autoload.php
+++ app/core/Autoload.php
@@ -1,31 +1,88 @@
 <?php
-namespace App\Core;
-
-
-class  AutoLoad
+//namespace App\Core;
+//
+//
+//class  AutoLoad
+//{
+//
+//    /**
+//     * @param $className
+//     * @return void
+//     */
+//    public static function autoLoad($className): void
+//    {
+//        $nameClass = explode('\\', $className);
+//
+//
+//        $className = str_replace("App", '', $className);
+//
+//
+//
+//        $className = explode('\\', $className);
+//
+//        $className = DS . strtolower($className[1]) . DS . $className[2] . ".php";
+//
+////        /core/Session.php
+////         "/opt/lampp/htdocs/CoursesNewspaper/app/core/Session.php"
+//
+//        if (file_exists(APP_PATH . $className)) {
+//            require APP_PATH . $className;
+//
+//        }
+//    }
+//}
+//
+//spl_autoload_register( __NAMESPACE__ . "\AutoLoad::autoLoad");
+class AutoLoad
 {
-    
+    private static array $config = [
+        'App' => APP_PATH,
+    ];
+
     /**
-     * @param $className
+     * convert first character to lower case in directories
+     * @param string $path path file
+     * @return string
+     */
+    private static function loweCaseDirectoryName(string $path): string
+    {
+        $path = explode(DS, $path);
+        $fileName = end($path);
+        $newPath = '';
+        for ($i = 0; $i < count($path) -1; $i++) {
+            $path[$i] = lcfirst($path[$i]);
+            $newPath .= $path[$i] . DS;
+        }
+
+        return $newPath . $fileName;
+    }
+
+    /**
+     * @param string $className
      * @return void
      */
-    public static function autoLoad($className): void
+    public static function autoLoad(string $className): void
     {
-        $nameClass = explode('\\', $className);
+        $className = ltrim($className, '\\');
+        
+        foreach (self::$config as $namespace => $baseDir) {
+
+            $namespacePrefix = $namespace . '\\';
 
 
-        $className = str_replace("App", '', $className);
+            if (str_starts_with($className, $namespacePrefix)) {
+                $relativeClassName = substr($className, strlen($namespacePrefix));
 
-        // $className = "\Core\Engine"
-        $className = explode('\\', $className);
+                $filePath = str_replace('\\', DS, $relativeClassName) . '.php';
+                $filePath = self::loweCaseDirectoryName($filePath);
+                $fullPath = $baseDir . DS . $filePath;
 
-        $className = DS . strtolower($className[1]) . DS . $className[2] . ".php";
-
-
-        if (file_exists(APP_PATH . $className)) {
-            require APP_PATH . $className;
+                if (file_exists($fullPath)) {
+                    require $fullPath;
+                }
+            }
         }
     }
 }
 
-spl_autoload_register( __NAMESPACE__ . "\AutoLoad::autoLoad");
+spl_autoload_register('\AutoLoad::autoLoad');
